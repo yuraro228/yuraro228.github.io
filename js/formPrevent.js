5c8e2811cff4847105b87df25064cc3c2845ca8a
yuraro228 <50003285+yuraro228@users.noreply.github.com>
2019-04-26 04:46:20 +0300

    some com

---
+++ js/formPrevent.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('sub').addEventListener('click', preven, false);
+
+
+function preven(evt) {
+    var c = document.getElementById('drop').value;
+    if (c == "") {
+        evt.preventDefault();
+    } 
+}
